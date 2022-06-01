232bc02518f63c74f6fcf717e36702851a68d5d1
gsbarbo <gagebarbour96@gmail.com>
2022-06-01 18:18:27 -0500

    added config settings for untouchables and skip perms

--- config/dms.php
+++ config/dms.php
@@ -10,4 +10,10 @@ return [
     // If members must fill out an application to join department. If false then admin must approve member.
     'must_apply' => false,
 
+    //steam_hexes of users that can skip permission checks. Be very careful
+    'skip_permissions' => [],
+
+    //steam_hexes of users that other users other than skip_permissions and super admin cant edit
+    'untouchable' => [],
+
 ];
