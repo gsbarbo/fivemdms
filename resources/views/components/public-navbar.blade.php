c32273a98a8769cf749684154938e20ddf7a591b
gsbarbo <gagebarbour96@gmail.com>
2022-06-08 22:28:50 -0500

    fixed route

--- resources/views/components/public-navbar.blade.php
+++ resources/views/components/public-navbar.blade.php
@@ -94,7 +94,7 @@
                             ?>
 
                             @if (auth()->user()->account_status == 4)
-                                <a href="{{ route('portal.index') }}"
+                                <a href="{{ route('portal.dashboard') }}"
                                     class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 hover:bg-gray-900">Go
                                     to Portal</a>
                             @endif
